5823eb1b79d476724e37c237d8e2ff928a77900b
RohitMazumder <30836968+RohitMazumder@users.noreply.github.com>
2019-05-04 21:16:36 +0530

    Reduced the number of states used in lexer

--- 8085 compiler/src/lexer.py
+++ 8085 compiler/src/lexer.py
@@ -1,26 +1,45 @@
 def lex(filecontents):
 	filecontents=list(filecontents)
 	tokens=[]
+	### Token codes used : ###
+	# ADR: Address String
+	# REG: Register
+	# VL8 : 8-bit Data
 	tok=""
 	string=""
 	state=0
+	d8=""
 	### State descriptions ###
-	# state = 0; Keywords and variables
-	# state = 1; String Address datatype
-	# state = 2: MVI
+	# state = 0: Keywords
+	# state = 1: String Address datatype
+	# state = 2: String register name
+	# state = 3: String 8-bit data
+	# state = 4: String 16-bit data
 	for c in filecontents:
 		tok=tok+c
 		if(tok==" "):
 			tok=""
 		elif (tok=="\n"):
 			if(state==1):
-				tokens.append("STR:"+string)
+				tokens.append("ADR:"+string)
 				string=""
+			elif(state==3):
+				tokens.append("VL8:"+d8)
+				d8=""
 			tok=""
 			state=0
+		elif(tok.isdigit()):
+			if(state==2):
+				state=3
+				d8+=c
+				tok=""
+			if(state==1):
+				string+=c
+
 		elif (tok=="STA"):
 			tokens.append("STA")
 			tok=""
+			state=1
 		elif(tok=="MVI"):
 			tokens.append("MVI")
 			tok=""
@@ -28,22 +47,18 @@ def lex(filecontents):
 		elif(tok=="MOV"):
 			tokens.append("MOV")
 			tok=""
-			state=3
+			state=2
 		elif(tok=="HLT"):
+			#print (tokens)
 			return tokens;
-		elif(tok.isdigit()):
-			state=1
-			tok=""
-			string+=c
 		elif (state==1):
 			string+=c
 			tok=""
 		elif (state==2):
 			tokens.append("REG:"+tok)
 			tok=""
-			state=1
 		elif(state==3):
-			tokens.append("REG:"+tok)
+			d8+=tok
 			tok=""
 	return tokens
 
