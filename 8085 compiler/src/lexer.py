97c232b5b773c388722836d934f039669f4981b3
RohitMazumder <30836968+RohitMazumder@users.noreply.github.com>
2019-05-04 11:29:20 +0530

    Update lexer.py

--- 8085 compiler/src/lexer.py
+++ 8085 compiler/src/lexer.py
@@ -7,27 +7,43 @@ def lex(filecontents):
 	### State descriptions ###
 	# state = 0; Keywords and variables
 	# state = 1; String Address datatype
-	# state = 2:
+	# state = 2: MVI
 	for c in filecontents:
 		tok=tok+c
 		if(tok==" "):
-			if(state==0):
-				tok=""
-		elif (tok=="\n"):
 			tok=""
+		elif (tok=="\n"):
+			if(state==1):
+				tokens.append("STR:"+string)
+				string=""
+			tok=""
+			state=0
 		elif (tok=="STA"):
 			tokens.append("STA")
 			tok=""
-		elif (tok=="\""):
-			if(state==0):
-				state=1
-			elif(state==1):
-				tokens.append("Ad:"+string)
-				tok=""
-				state=0
-				string=""
+		elif(tok=="MVI"):
+			tokens.append("MVI")
+			tok=""
+			state=2
+		elif(tok=="MOV"):
+			tokens.append("MOV")
+			tok=""
+			state=3
+		elif(tok=="HLT"):
+			return tokens;
+		elif(tok.isdigit()):
+			state=1
+			tok=""
+			string+=c
 		elif (state==1):
 			string+=c
+			tok=""
+		elif (state==2):
+			tokens.append("REG:"+tok)
+			tok=""
+			state=1
+		elif(state==3):
+			tokens.append("REG:"+tok)
 			tok=""
 	return tokens
 
